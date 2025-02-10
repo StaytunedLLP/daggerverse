ad6700f5628403f558dabc6f9ad9eb2a63a5fc16
Abhayraj Yadav <abhayraj.yadav.st4@staytuned.company>
2025-02-10 16:58:30 +0530

    Initialize Dagger configuration and GitDiffFiles module
    dagger | #1 | dev

---
+++ .dagger/src/index.ts
@@ -0,0 +1,43 @@
+/**
+ * A generated module for GitDiffFiles functions
+ *
+ * This module has been generated via dagger init and serves as a reference to
+ * basic module structure as you get started with Dagger.
+ *
+ * Two functions have been pre-created. You can modify, delete, or add to them,
+ * as needed. They demonstrate usage of arguments and return types using simple
+ * echo and grep commands. The functions can be called from the dagger CLI or
+ * from one of the SDKs.
+ *
+ * The first line in this comment block is a short description line and the
+ * rest is a long description with more detail on the module's purpose or usage,
+ * if appropriate. All modules should have a short description.
+ */
+import { dag, Container, Directory, object, func } from "@dagger.io/dagger"
+
+@object()
+export class GitDiffFiles {
+  /**
+   * Returns an array of files in the staged state
+   */
+  @func()
+  async getStagedFiles(source: Directory): Promise<string[]> {
+    const container = dag
+      .container()
+      .from("alpine/git:latest")
+      .withDirectory("/app", source)
+      .withWorkdir("/app")
+
+    const changedFilesOutput = await container
+      .withExec(["git", "diff", "--cached", "--name-only", "--diff-filter=ACMR"])
+      .stdout();
+
+    // Split the output into an array of files, removing any empty entries
+    const files = changedFilesOutput
+      .split("\n")
+      .map((file) => file.trim())
+      .filter(Boolean);
+
+    return files;
+  }
+}
